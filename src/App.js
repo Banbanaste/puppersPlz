cb84f33219682e2e1652db87982a603b61158b69
Roberto Banbanaste <rbanbanaste@outlook.com>
2019-09-20 03:22:20 -0400

    refactor ideation, fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
 const API = "https://dog.ceo/api/breeds/list/all";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      breeds: []
+      breeds: [],
+      breedImgaes: [],
+      isFetching: false
     };
   }
   componentDidMount() {
+    this.setState({ isFetching: true });
     fetch(API)
       .then(response => response.json())
       .then(data => data.message)
       .then(breedObject => this.getNamesFromObject(breedObject))
       .then(breeds => this.getLinksFromArray(breeds))
-      .then(apiArray => this.fetchImages(apiArray));
+      .then(apiArray => this.fetchImages(apiArray))
+      .then(thing => {
+        this.setState({ breedImgaes: thing });
+      });
+    this.setState({ isFetching: false });
   }
   getNamesFromObject = object => {
     let breeds = [];
-    for (let breedName in object) {
+    for (var breedName in object) {
       if (object[breedName].length !== 0) {
         breeds.push(breedName);
         object[breedName].forEach(subBreed => {
@@ -51,14 +59,37 @@ class App extends Component {
     return breedAPIArray;
   };
   fetchImages = array => {
+    let someArray = [];
     array.forEach(api => {
-      fetch(api)
+      return fetch(api)
         .then(response => response.json())
-        .then(data => console.log(data.message));
+        .then(data => data.message)
+        .then(listImgSrc => someArray.push(listImgSrc));
     });
+    return someArray;
   };
+  generateListItem = url => {
+    console.log(url);
+    let listItem = (
+      <li>
+        <img src={url} alt="missing pup" />
+      </li>
+    );
+    return listItem;
+  };
+
   render() {
-    return <h1>Hello, World!</h1>;
+    console.log(this.state.breedImgaes);
+    return (
+      <div>
+        <Header />
+        <ul>
+          {this.state.breedImgaes.forEach(img => {
+            this.generateListItem(img);
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 export default App;
